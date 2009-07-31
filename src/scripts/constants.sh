ca017368e0f5e67b8c03d0e6c5fa6eafc22d3bca
Daniel Roberts <Ademan555@gmail.com>
2009-07-31 12:09:37 -0700

    Deleted constants.sh by accident, need to commit to check it out haha.

--- src/scripts/constants.sh
+++ src/scripts/constants.sh
@@ -1,38 +1,11 @@
 #! /bin/bash
 
-function constant
+function read_constants
 {
-    echo '$1 = c_uint($2)'
-}
-
-function get_requests
-{
-    echo "$1"
-    sed -nre '/^#\s+Begin\s+GL\s+Constants$/,/^#End\s+GL\s+Constants$/s/^#\s*GL_([A-Z0-9_]+)\s*$/\1/p' $1
-}
-
-function append_constant
-{
-    echo "$(cat)|$1"
-}
-
-function retrieve_constants
-{
-    i=0
-    cat | while read request; do
-       constants=$(echo "$constants" | append_constant $request)
-    done
-
-    constants=$(egrep "#define[[:space:]]+($constants)" $1 |\
+    egrep "#define[[:space:]]+([A-Z0-9_]+)" $1 |\
     sed -re 's/^#define\s+(GL_[A-Z0-9_]+)\s+(0x[0-9A-Fa-f]+)/\1="\2"/' |\
-    grep -v '#define')
-    $constants
+    grep -v '#define'
 }
 
-#FIXME: sed character classes?
-#echo 'from ctypes import c_uint'
-#egrep '#define[[:space:]]+GL' $1 | sed -re 's/^#define\s+(GL_[A-Z0-9_]+)\s+(0x[0-9A-Fa-f]+)/\1 = c_uint(\2)/' | grep -v '#define'
-
-#get_requests $1
-
-retrieve_constants $1
+echo 'from gltypes import GLenum'
+read_constants $1 | sed -re 's/="((0x)?[0-9A-fa-f)"/=GLenum(\1)'
